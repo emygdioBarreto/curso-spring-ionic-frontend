48f8f0e7a4393d11e9c4b74db7638ec86cfa5b8f
Emygdio Barreto <emygdio.barreto@gmail.com>
2020-09-26 05:25:47 -0300

    Tela de confirmacao de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -12,7 +12,11 @@ export class ClienteService {
     }
 
     findByEmail(email: string) {
-         return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    }
+
+    findById(id: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
     }
 
     getImageFromBucket(id : string) : Observable<any> {
