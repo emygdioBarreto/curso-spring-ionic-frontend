145ab3d923102d5c575e95139afc0ab4d1845d78
Emygdio Barreto <emygdio.barreto@gmail.com>
2020-09-26 14:50:38 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,6 +26,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -35,11 +39,11 @@ export class ProfilePage {
         },
         error => {
           if (error.status == 403) {
-            this.navCtrl.setRoot('/home');
+            this.navCtrl.setRoot('HomePage');
           }
         });
     } else {
-      this.navCtrl.setRoot('/home');
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
@@ -65,5 +69,19 @@ export class ProfilePage {
       this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {        
+      });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 }
